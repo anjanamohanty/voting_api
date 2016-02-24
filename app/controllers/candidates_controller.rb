528fad46eb8906bccf34c096a3994462c74aec51
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-23 22:20:26 -0500

    Create index, show, create methods for Candidates controller

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,13 @@
 class CandidatesController < ApplicationController
   def index
+    render json: Candidate.all
   end
 
   def show
+    render json: Candidate.first
   end
 
   def create
+    Candidate.create!(name: params["name"], district: "Durham County", party: "Independent", hometown: "Durham")
   end
 end
