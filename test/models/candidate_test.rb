e63d46dd67a6f731ca0ca0d6f9a64f94fe7eeb25
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-23 21:42:32 -0500

    Add unit tests for Race model

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -12,7 +12,7 @@ class CandidateTest < ActiveSupport::TestCase
   test "candidates have many votes" do
     v = Vote.create!(voter_id: 2)
     v_two = Vote.create!(voter_id: 3)
-    c = Candidate.create(name: "Camden Carver", district: "Durham County", hometown: "Oxford", party: "Independent")
+    c = Candidate.create!(name: "Camden Carver", district: "Durham County", hometown: "Oxford", party: "Independent")
 
     c.votes << v
     c.votes << v_two
--- test/models/race_test.rb
+++ test/models/race_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class RaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create races" do
+    assert Race.create!(name: "2016 Durham County")
+  end
+
+  test "races have many votes" do
+    v = Vote.create!(voter_id: 2)
+    v_two = Vote.create!(voter_id: 3)
+    r = Race.create!(name: "2016 Durham County")
+
+    r.votes << v
+    r.votes << v_two
+
+    assert_equal 2, r.votes.count
+  end
+
+  test "races have many candidates" do
+    c = Candidate.create!(name: "Camden Carver", district: "Durham County", hometown: "Oxford", party: "Independent")
+    c_two = Candidate.create!(name: "Mike Hourigan", district: "Durham County", hometown: "Huntersville", party: "Democrat")
+    r = Race.create!(name: "2016 Durham County")
+
+    r.candidates << c
+    r.candidates << c_two
+
+    assert_equal 2, r.candidates.count
+  end
 end
