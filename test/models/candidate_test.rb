4831453b5895fb785621b1c030caca5d90174d38
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-23 21:33:19 -0500

    Add unit tests for Candidate model

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create candidates" do
+    assert Candidate.create!(name: "Camden Carver", district: "Durham County", hometown: "Oxford", party: "Independent")
+  end
+
+  test "cannot create candidates with missing params" do
+    refute Candidate.create(name: "Camden Carver").valid?
+  end
+
+  test "candidates have many votes" do
+    v = Vote.create!(voter_id: 2)
+    v_two = Vote.create!(voter_id: 3)
+    c = Candidate.create(name: "Camden Carver", district: "Durham County", hometown: "Oxford", party: "Independent")
+
+    c.votes << v
+    c.votes << v_two
+
+    assert_equal 2, c.votes.count
+  end
 end
