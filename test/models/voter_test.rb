f681d29b744fca91001abe307f900cf9a16d3038
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-23 22:00:21 -0500

    Add unit tests for Voter model

--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class VoterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create voter" do
+    assert Voter.create!(name: "Anjana Mohanty", party: "Independent", token: "abc1234")
+  end
+
+  test "voter has one and only one vote" do
+    v = Voter.create!(name: "Anjana Mohanty", party: "Independent", token: "abc1234")
+    vote = Vote.create!
+    v.vote = vote
+
+    assert_equal vote.voter_id, v.id
+    refute Vote.create(voter_id: "#{v.id}").valid?
+  end
 end
